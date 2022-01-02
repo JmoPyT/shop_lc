2b67675305a93d0413ce5e1cb73c9ec6505a7854
Jmo PyT <15142053+JmoPyT@users.noreply.github.com>
2022-01-02 13:45:09 +0100

    [FEAT] day unit tests

---
+++ spec/models/day_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe Day, type: :model do
+    describe "#valid" do
+        context 'presence' do
+            it 'should be valid' do
+                rspec_day = Day.create(label: 'Monday')
+                expect(rspec_day).to be_valid
+            end
+
+            it 'should be not valid' do
+                rspec_day = Day.create(label: '')
+                expect(rspec_day).not_to be_valid
+            end
+        end
+
+        context 'uniqueness' do
+            it 'should be not valid' do
+                rspec_day1 = Day.create!(label: 'Monday')
+                rspec_day2 = Day.create(label: 'Monday')
+                expect(rspec_day2).not_to be_valid
+            end
+        end
+    end
+end
